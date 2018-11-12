02e632106b7f744da1d91bf8d28c1d2ffce0dfae
mazendev <mazen_musa2000@yahoo.com>
2018-11-12 00:03:34 -0500

    Update header.php

--- header.php
+++ header.php
@@ -35,14 +35,14 @@
   <body id="page-top">
 
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg bg-dark" id="mainNav">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="mainNav">
       <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="#page-top">MJTravel</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
+          <ul class="navbar-nav ml-auto text-white">
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="/myplugins">Home</a>
             </li>
